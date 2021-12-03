febcd63547d798ef2a803ebd71ca3d8786b13664
void0l <95478111+void0l@users.noreply.github.com>
2021-12-03 13:20:51 +0100

    Update Extensions.cs
    code cleanup

--- TerraformRimworld/Source/v1.3_TerraformRimworld/Extensions.cs
+++ TerraformRimworld/Source/v1.3_TerraformRimworld/Extensions.cs
@@ -1,4 +1,4 @@
-﻿using RimWorld;
+using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -7,100 +7,99 @@ using Verse;
 namespace TerraformRimworld
 {
 	public static class Extensions
-    {
-        #region extend terrain
-        public static List<string> GetNaturalTerrains()
-        {
-            List<string> l = new List<string>();
-            l.Add("Soil");
-            l.Add("SoilRich");
-            l.Add("MossyTerrain");
-            l.Add("MarshyTerrain");
-            l.Add("Gravel");
-            l.Add("Mud");
-            l.Add("Sand");
-            l.Add("SoftSand");
-            l.Add("BrokenAsphalt");
-            l.Add("PackedDirt");
-            l.Add("Ice");                                    
-            l.Add("Marsh");                     
-            l.Add("WaterShallow");
-            l.Add("WaterOceanShallow");
-            l.Add("WaterMovingChestDeep");
-            l.Add("WaterMovingShallow");
-            l.Add("WaterDeep");
-            l.Add("WaterOceanDeep");
-            return l;
-        }
+	{
+		#region extend terrain
 
-        public static List<string> GetLiquidTerrains()
-        {
-            List<string> l = new List<string>();
-            l.Add("Marsh");            
-            l.Add("WaterShallow");
-            l.Add("WaterOceanShallow");
-            l.Add("WaterMovingChestDeep");
-            l.Add("WaterMovingShallow");
-            l.Add("WaterDeep");
-            l.Add("WaterOceanDeep");
-            return l;
-        }
+		public static List<string> GetBasicTerrains()
+		{
+			List<string> l = new List<string>();
+			l.Add("Soil");
+			l.Add("SoilRich");
+			l.Add("MossyTerrain");
+			l.Add("MarshyTerrain");
+			l.Add("Gravel");
+			l.Add("Mud");
+			l.Add("Sand");
+			l.Add("SoftSand");
+			l.Add("BrokenAsphalt");
+			l.Add("PackedDirt");
+			l.Add("Ice");
+			return l;
+		}
 
-        public static List<string> GetBasicTerrains()
-        {
-            List<string> l = new List<string>();
-            l.Add("Soil");
-            l.Add("SoilRich");
-            l.Add("MossyTerrain");
-            l.Add("MarshyTerrain");
-            l.Add("Gravel");
-            l.Add("Mud");
-            l.Add("Sand");
-            l.Add("SoftSand");
-            l.Add("BrokenAsphalt");
-            l.Add("PackedDirt");
-            l.Add("Ice");
-            return l;
-        }
+		public static List<string> GetLiquidTerrains()
+		{
+			List<string> l = new List<string>();
+			l.Add("Marsh");
+			l.Add("WaterShallow");
+			l.Add("WaterOceanShallow");
+			l.Add("WaterMovingChestDeep");
+			l.Add("WaterMovingShallow");
+			l.Add("WaterDeep");
+			l.Add("WaterOceanDeep");
+			return l;
+		}
 
+		public static List<string> GetNaturalTerrains()
+		{
+			List<string> l = new List<string>();
+			l.Add("Soil");
+			l.Add("SoilRich");
+			l.Add("MossyTerrain");
+			l.Add("MarshyTerrain");
+			l.Add("Gravel");
+			l.Add("Mud");
+			l.Add("Sand");
+			l.Add("SoftSand");
+			l.Add("BrokenAsphalt");
+			l.Add("PackedDirt");
+			l.Add("Ice");
+			l.Add("Marsh");
+			l.Add("WaterShallow");
+			l.Add("WaterOceanShallow");
+			l.Add("WaterMovingChestDeep");
+			l.Add("WaterMovingShallow");
+			l.Add("WaterDeep");
+			l.Add("WaterOceanDeep");
+			return l;
+		}
+		public static bool IsBasic(this TerrainDef terrain)
+		{
+			return GetBasicTerrains().Contains(terrain.defName);
+		}
 
-        public static bool IsNatural(this TerrainDef terrain)
-        {
-            return GetNaturalTerrains().Contains(terrain.defName);
-        }
+		public static bool IsExotic(this TerrainDef terrain)
+		{
+			return (terrain.defName != null && (terrain.defName.StartsWith("Exotic") || terrain.defName == "Moon" || terrain.defName == "MetalRim" || terrain.defName == "IceDeep"));
+		}
 
-        public static bool IsLiquid(this TerrainDef terrain)
-        {
-            return GetLiquidTerrains().Contains(terrain.defName);
-        }
+		public static bool IsLava(this TerrainDef terrain)
+		{
+			return (terrain.defName != null && terrain.defName.StartsWith("Lava"));
+		}
 
-        public static bool IsBasic(this TerrainDef terrain)
-        {
-            return GetBasicTerrains().Contains(terrain.defName);
-        }
+		public static bool IsLiquid(this TerrainDef terrain)
+		{
+			return GetLiquidTerrains().Contains(terrain.defName);
+		}
 
-        public static bool IsLava(this TerrainDef terrain)
-        {
-            return (terrain.defName != null && terrain.defName.StartsWith("Lava"));
-        }
+		public static bool IsNatural(this TerrainDef terrain)
+		{
+			return GetNaturalTerrains().Contains(terrain.defName);
+		}
+		public static bool IsOther(this TerrainDef terrain)
+		{
+			return TRMod.OPTION_UseAllTerrain || (!terrain.IsNatural() && !terrain.IsRocky() && !terrain.IsExotic() && !terrain.IsLava() && !terrain.IsCarpet && terrain.designationCategory == null && terrain.designatorDropdown == null);
+		}
 
-        public static bool IsRocky(this TerrainDef terrain)
-        {
-            return (terrain.defName != null && (terrain.defName.EndsWith("_Smooth") || terrain.defName.EndsWith("_RoughHewn") || terrain.defName.EndsWith("_Rough")));
-        }
-
-        public static bool IsExotic(this TerrainDef terrain)
-        {
-            return (terrain.defName != null && (terrain.defName.StartsWith("Exotic") || terrain.defName == "Moon" || terrain.defName == "MetalRim" || terrain.defName == "IceDeep"));
-        }
-
-        public static bool IsOther(this TerrainDef terrain)
-        {
-            return TRMod.OPTION_UseAllTerrain || (!terrain.IsNatural() && !terrain.IsRocky() && !terrain.IsExotic() && !terrain.IsLava() && !terrain.IsCarpet && terrain.designationCategory == null && terrain.designatorDropdown == null);
-        }
-		#endregion
+		public static bool IsRocky(this TerrainDef terrain)
+		{
+			return (terrain.defName != null && (terrain.defName.EndsWith("_Smooth") || terrain.defName.EndsWith("_RoughHewn") || terrain.defName.EndsWith("_Rough")));
+		}
+		#endregion extend terrain
 
 		#region extend intvec
+
 		public static ThingDef GetFilthFromPos(this IntVec3 pos, Map map)
 		{
 			List<Thing> thingList = pos.GetThingList(map);
@@ -112,47 +111,16 @@ namespace TerraformRimworld
 			}
 			return null;
 		}
-		#endregion
+
+		#endregion extend intvec
 
 		#region extend thingdef
-		public static bool IsPlant(this Thing thing)
-		{
-			return (thing != null && thing.def != null && thing.def.defName != null && thing.def.plant != null);
-		}
 
-		public static bool IsPlant(this ThingDef td)
-		{
-			return (td != null && td.defName != null && td.plant != null);
-		}
-
-		public static bool IsMineableRock(this ThingDef td)
-		{
-			return (td != null && td.defName != null && td.building != null && td.building.mineableThing != null && !td.building.isResourceRock);
-		}
-
-		public static bool IsMineableMineral(this ThingDef td)
-		{
-			return (td != null && td.defName != null && td.building != null && td.building.mineableThing != null && td.building.isResourceRock);
-		}
-
-		public static void UpdateCosts(this ThingDef td, Dictionary<ThingDef, int> dicCosts)
+		public static void AddCosts(this ThingDef td, ThingDef costDef, int value)
 		{
 			if (td.costList == null)
 				td.costList = new List<ThingDefCountClass>();
-			else
-				td.costList.Clear();
-
-			foreach (ThingDef t in dicCosts.Keys)
-				td.AddCosts(t, dicCosts[t]);
-		}
-
-		public static void UpdateStat(this ThingDef td, string statDefName, float value)
-		{
-			foreach (StatModifier sm in td.statBases)
-			{
-				if (sm.stat != null && sm.stat.defName == statDefName)
-					sm.value = value;
-			}
+			td.costList.Add(new ThingDefCountClass(costDef, value));
 		}
 
 		public static void AddStatBase(this ThingDef td, StatDef statDef, float value)
@@ -165,22 +133,25 @@ namespace TerraformRimworld
 			td.statBases.Add(sm);
 		}
 
-		public static void AddCosts(this ThingDef td, ThingDef costDef, int value)
+		public static bool IsMineableMineral(this ThingDef td)
 		{
-			if (td.costList == null)
-				td.costList = new List<ThingDefCountClass>();
-			td.costList.Add(new ThingDefCountClass(costDef, value));
+			return (td != null && td.defName != null && td.building != null && td.building.mineableThing != null && td.building.isResourceRock);
 		}
 
-		public static void SetGraphicDataSingle(this ThingDef td, string texPath, string uiTexPath)
+		public static bool IsMineableRock(this ThingDef td)
 		{
-			td.graphicData = new GraphicData();
-			td.graphicData.texPath = texPath;
-			td.graphicData.shaderType = ShaderTypeDefOf.MetaOverlay;
-			td.graphicData.graphicClass = typeof(Graphic_Single);
-			td.uiIconPath = uiTexPath;			
+			return (td != null && td.defName != null && td.building != null && td.building.mineableThing != null && !td.building.isResourceRock);
 		}
 
+		public static bool IsPlant(this Thing thing)
+		{
+			return (thing != null && thing.def != null && thing.def.defName != null && thing.def.plant != null);
+		}
+
+		public static bool IsPlant(this ThingDef td)
+		{
+			return (td != null && td.defName != null && td.plant != null);
+		}
 		public static void RegisterBuildingDef(this ThingDef td)
 		{
 			if (DefDatabase<ThingDef>.GetNamed(td.defName, false) != null)
@@ -206,9 +177,8 @@ namespace TerraformRimworld
 				minifiedDef.PostLoad();
 			}
 
-    		td.ResolveReferences();
+			td.ResolveReferences();
 			td.PostLoad();
-
 
 			MethodInfo shortHashGiver = typeof(ShortHashGiver).GetMethod(name: "GiveShortHash", bindingAttr: BindingFlags.NonPublic | BindingFlags.Static) ?? throw new ArgumentNullException();
 			Type t = typeof(ThingDef);
@@ -224,65 +194,98 @@ namespace TerraformRimworld
 			DefDatabase<ThingDef>.Add(td.blueprintDef);
 			DefDatabase<ThingDef>.Add(td.frameDef);
 		}
-		#endregion
+
+		public static void SetGraphicDataSingle(this ThingDef td, string texPath, string uiTexPath)
+		{
+			td.graphicData = new GraphicData();
+			td.graphicData.texPath = texPath;
+			td.graphicData.shaderType = ShaderTypeDefOf.MetaOverlay;
+			td.graphicData.graphicClass = typeof(Graphic_Single);
+			td.uiIconPath = uiTexPath;
+		}
+
+		public static void UpdateCosts(this ThingDef td, Dictionary<ThingDef, int> dicCosts)
+		{
+			if (td.costList == null)
+				td.costList = new List<ThingDefCountClass>();
+			else
+				td.costList.Clear();
+
+			foreach (ThingDef t in dicCosts.Keys)
+				td.AddCosts(t, dicCosts[t]);
+		}
+
+		public static void UpdateStat(this ThingDef td, string statDefName, float value)
+		{
+			foreach (StatModifier sm in td.statBases)
+			{
+				if (sm.stat != null && sm.stat.defName == statDefName)
+					sm.value = value;
+			}
+		}
+		#endregion extend thingdef
 
 		#region extend string
+
 		public static string SubstringFrom(this String text, string from, int occuranceCount)
-        {
-            string temp = text;
-            for (int i = 0; i < occuranceCount; i++)
-                temp = temp.SubstringFrom(from);
-            return temp;
-        }
-        public static string SubstringTo(this String text, string to, int occuranceCount)
-        {
-            string temp = text;
-            string sub = "";
-            for (int i = 0; i < occuranceCount; i++)
-            {
-                sub += temp.SubstringTo(to, false);
-                temp = temp.SubstringFrom(to);
-            }
-            return sub;
-        }
-        /// <summary>
-        /// Teilstring ab dem angegebenen 'startFrom' String
-        /// </summary>
-        /// <param name="text"></param>
-        /// <param name="startFrom"></param>
-        /// <returns></returns>
-        public static string SubstringFrom(this String text, string startFrom, bool withoutIt = true)
-        {
-            int pos = text.IndexOf(startFrom);
-            if (pos >= 0)
-            {
-                if (withoutIt)
-                    return text.Substring(pos + startFrom.Length);
-                else
-                    return text.Substring(pos);
-            }
-            else
-                return text;
-        }
-        /// <summary>
-        /// Teilstring bis zum angegebenen 'endOn' String
-        /// </summary>
-        /// <param name="text"></param>
-        /// <param name="endOn"></param>
-        /// <returns></returns>
-        public static string SubstringTo(this String text, string endOn, bool withoutIt = true)
-        {
-            int pos = text.IndexOf(endOn);
-            if (pos >= 0)
-            {
-                if (withoutIt)
-                    return text.Substring(0, pos);
-                else
-                    return text.Substring(0, pos + endOn.Length);
-            }
-            else
-                return text;
-        }
-		#endregion
-    }
+		{
+			string temp = text;
+			for (int i = 0; i < occuranceCount; i++)
+				temp = temp.SubstringFrom(from);
+			return temp;
+		}
+
+		/// <summary>
+		/// Teilstring ab dem angegebenen 'startFrom' String
+		/// </summary>
+		/// <param name="text"></param>
+		/// <param name="startFrom"></param>
+		/// <returns></returns>
+		public static string SubstringFrom(this String text, string startFrom, bool withoutIt = true)
+		{
+			int pos = text.IndexOf(startFrom);
+			if (pos >= 0)
+			{
+				if (withoutIt)
+					return text.Substring(pos + startFrom.Length);
+				else
+					return text.Substring(pos);
+			}
+			else
+				return text;
+		}
+
+		public static string SubstringTo(this String text, string to, int occuranceCount)
+		{
+			string temp = text;
+			string sub = "";
+			for (int i = 0; i < occuranceCount; i++)
+			{
+				sub += temp.SubstringTo(to, false);
+				temp = temp.SubstringFrom(to);
+			}
+			return sub;
+		}
+		/// <summary>
+		/// Teilstring bis zum angegebenen 'endOn' String
+		/// </summary>
+		/// <param name="text"></param>
+		/// <param name="endOn"></param>
+		/// <returns></returns>
+		public static string SubstringTo(this String text, string endOn, bool withoutIt = true)
+		{
+			int pos = text.IndexOf(endOn);
+			if (pos >= 0)
+			{
+				if (withoutIt)
+					return text.Substring(0, pos);
+				else
+					return text.Substring(0, pos + endOn.Length);
+			}
+			else
+				return text;
+		}
+
+		#endregion extend string
+	}
 }
